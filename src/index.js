76bceae402c6c5a0d96a8b058e4abea6664674e4
AnalieFS <analieflorezs@gmail.com>
2022-07-07 16:34:42 -0500

    Cors Policy

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const userContact = require('./Routes/Contact');
+const cors = require('cors');
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 9000;
 
 //Middleware config
+app.use(cors());
 app.use(express.json());
 app.use('/api', userContact);
 
